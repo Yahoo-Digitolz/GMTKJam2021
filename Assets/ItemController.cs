26e7da9601909057d9da80edc52956415be8ef79
yodaxy <yodaxy@hotmail.com>
2021-06-17 19:08:40 +0200

    Key
    - graphismes
    - animations
    - comportement (event quand on passe dessus qui active ce qu'on veut)

---
+++ Assets/ItemController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ItemController : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _onCollect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            _onCollect.Invoke();
+        }
+    }
+}
